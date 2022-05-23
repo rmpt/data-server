6b76cd932eae307e461a158e524722a6b39bdda3
Rui Teixeira <ruiteixeira.dev@gmail.com>
2022-05-23 10:55:47 +0100

    CORS enabled for any origin

---
+++ src/main/kotlin/rmpt/dataserver/Config.kt
@@ -0,0 +1,14 @@
+package rmpt.dataserver
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class Config : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+    }
+}
